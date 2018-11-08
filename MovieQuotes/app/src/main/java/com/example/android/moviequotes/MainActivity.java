d4dec03c0f4b6cdb3617a9caf60d1923ee5fc1e5
senanoc <senanoc@gmail.com>
2018-11-08 20:24:48 +0000

    functional unsorted CR

--- MovieQuotes/app/src/main/java/com/example/android/moviequotes/MainActivity.java
+++ MovieQuotes/app/src/main/java/com/example/android/moviequotes/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.moviequotes;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,11 +12,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    //public static final String TAG = "MQ";
-    private int mTempCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,43 +39,34 @@ public class MainActivity extends AppCompatActivity {
         MovieQuoteAdapter movieQuoteAdapter = new MovieQuoteAdapter();
         recyclerView.setAdapter(movieQuoteAdapter);
 
-
-        // Temp Firebase testing area
-
-//        final FirebaseFirestore db = FirebaseFirestore.getInstance();
-//
-//        db.collection("moviequotes")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                Log.d(TAG, document.getId() + " => " + document.getData());
-//                            }
-//                        } else {
-//                            Log.w(TAG, "Error getting documents.", task.getException());
-//                        }
-//                    }
-//                });
-
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Add Firestore Doc", Snackbar.LENGTH_LONG).show();
+                showAddDialog();
 
-                // Create a new mq with a first and last name
-//                Map<String, Object> mq = new HashMap<>();
-//                mTempCounter = mTempCounter + 1;
-//                mq.put("quote", "Quote #" + mTempCounter);
-//                mq.put("movie", "Movie #" + mTempCounter);
-//
-//// Add a new document with a generated ID
-//                db.collection("moviequotes").add(mq);
             }
         });
+    }
+
+    private void showAddDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.moviequote_dialog, null, false);
+        builder.setView(view);
+        final TextView quoteEditText = view.findViewById(R.id.dialog_quote_edittext);
+        final TextView movieEditText = view.findViewById(R.id.dialog_movie_edittext);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Map<String, Object> mq = new HashMap<>();
+                mq.put(Constants.KEY_QUOTE, quoteEditText.getText().toString());
+                mq.put(Constants.KEY_MOVIE, movieEditText.getText().toString());
+                FirebaseFirestore.getInstance().collection(Constants.COLLECTION_PATH).add(mq);
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel,null);
+
+        builder.create().show();
     }
 
     @Override
